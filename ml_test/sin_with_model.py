cdb568e9c788f1a5c32d38088363fda89622cec5
khgxsss <doodoo1113@gmail.com>
2023-03-23 10:22:57 +0900

    modified:   cores/__pycache__/__init__.cpython-39.pyc 	modified:   cores/__pycache__/core.cpython-39.pyc 	modified:   cores/__pycache__/functions.cpython-39.pyc 	modified:   cores/__pycache__/utils.cpython-39.pyc 	modified:   cores/models.py 	new file:   ml_test/sin_with_model.py

---
+++ ml_test/sin_with_model.py
@@ -0,0 +1,56 @@
+if '__file__' in globals():
+    import os, sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import numpy as np
+import matplotlib.pyplot as plt
+from cores import Model
+import cores.functions as F
+import cores.layers as L
+
+# 데이터셋
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = np.sin(2 * np.pi * x) + np.random.rand(100,1) # sin func
+
+# 하이퍼파라미터 설정
+
+lr = 0.2
+iters = 10000
+hidden_size = 10
+
+# 모델 정의
+class TwoLayerNet(Model):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.l1 = L.Linear(hidden_size)
+        self.l2 = L.Linear(out_size)
+    
+    def forward(self, x):
+        y = F.sigmoid(self.l1(x))
+        y = self.l2(y)
+        return y
+
+model = TwoLayerNet(hidden_size, 1)
+        
+# 신경망 학습
+
+for i in range(iters):
+    y_pred = model(x)
+    loss = F.mean_squared_error(y, y_pred)
+
+    model.cleargrads()
+    loss.backward()
+
+    for p in model.params():
+        p.data -= lr * p.grad.data
+    if i % 1000 == 0:
+        print(loss)
+
+# Plot
+plt.scatter(x, y, s=10)
+plt.xlabel('x')
+plt.ylabel('y')
+t = np.arange(0, 1, .01)[:, np.newaxis]
+y_pred = model(t)
+plt.plot(t, y_pred.data, color='r')
+plt.show()
